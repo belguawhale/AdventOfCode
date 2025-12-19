6486c14ab6b62c193db6a7b4df63201706b0fdab
Belgua Whale <belguawhale@gmail.com>
2025-12-19 02:58:20 -0500

    day 4 part 2

--- 2025/day_4/day_4.py
+++ 2025/day_4/day_4.py
@@ -4,13 +4,13 @@ import math
 
 def parse_file(file: str):
     with open(file, "r") as f:
-        return [l.strip() for l in f]
+        return [list(l.strip()) for l in f]
 
-def part_1(grid: List[List[str]], debug=False):
+def accessible_papers(grid: List[List[str]]):
+    can_access = []
     height = len(grid)
     width = len(grid[0])
     num_neighbour_paper = [[0] * width for _ in range(height)]
-    can_access = 0
 
     neighbours = [(x, y) for x in range(-1, 2) for y in range(-1, 2) if x or y]
     for r in range(height):
@@ -29,8 +29,12 @@ def part_1(grid: List[List[str]], debug=False):
     for r in range(height):
         for c in range(width):
             if grid[r][c] == "@" and num_neighbour_paper[r][c] < 4:
-                can_access += 1
+                can_access.append((r, c))
     return can_access
+
+
+def part_1(grid: List[List[str]], debug=False):
+    return len(accessible_papers(grid))
 
 
 print("===== Part 1 =====")
@@ -56,3 +60,27 @@ def test(cases, fn):
 
 
 test(test_cases_part_1, part_1)
+
+print("===== Part 2 =====")
+
+def part_2(grid: List[List[str]]):
+    total = 0
+    num_accessible = 1
+    # lazy solution, run part 1 in a loop to identify accessible paper and remove them
+    while num_accessible > 0:
+        accessible = accessible_papers(grid)
+        num_accessible = len(accessible)
+        total += num_accessible
+        for r, c in accessible:
+            grid[r][c] = "."
+    return total
+
+example = parse_file("input_example.txt")
+print(part_2(example))
+
+inp = parse_file("input.txt")
+print(part_2(inp))
+
+test_cases_part_2 = [
+    [parse_file("input_example.txt"), 43],
+]
