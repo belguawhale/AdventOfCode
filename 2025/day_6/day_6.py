24354e4819c4681c5ff9ea8255cbaf8594797e93
Belgua Whale <belguawhale@gmail.com>
2025-12-23 04:50:54 -0500

    day 6 part 2

--- 2025/day_6/day_6.py
+++ 2025/day_6/day_6.py
@@ -51,18 +51,56 @@ def test(cases, fn):
 
 
 test(test_cases_part_1, part_1)
-exit()
 
 print("===== Part 2 =====")
 
-example = parse_file("input_example.txt")
+
+def parse_file2(file: str) -> InputType:
+    # columns are divided based on the index to the left of each operator
+    # 123 456
+    #  78 9
+    # *   +
+    #    ^
+    with open(file, "r") as f:
+        lines = f.readlines()
+    op_line = lines.pop().strip("\n")
+    ops = re.split("\s+", op_line)
+
+    # determine ending indexes of each column, e.g.
+    # 123 234 34 45
+    # *   +   *  +
+    #   ^   ^  ^  ^
+    positions = [group.end() - 3 for group in re.finditer(r"[*+]", op_line)]
+    positions = positions[1:] + [len(op_line) - 1]
+
+    rows = []
+    for line in lines:
+        chunks = []
+        start_pos = 0
+        for end_pos in positions:
+            chunks.append(line[start_pos : end_pos + 1])
+            start_pos = end_pos + 2
+        rows.append(chunks)
+    # print("r", rows)
+    cols = list(zip(*rows))
+    # print("c", cols)
+    vert_nums = [[int("".join(s).strip()) for s in zip(*c)] for c in cols]
+    # print("v", vert_nums)
+    return list(zip(vert_nums, ops))
+
+
+def part_2(inp: InputType, debug=False):
+    return part_1(inp, debug)
+
+
+example = parse_file2("input_example.txt")
 print(part_2(example))
 
-inp = parse_file("input.txt")
+inp = parse_file2("input.txt")
 print(part_2(inp))
 
 test_cases_part_2 = [
-    [parse_file("input_example.txt"), 14],
+    [parse_file2("input_example.txt"), 3263827],
 ]
 
 test(test_cases_part_2, part_2)
