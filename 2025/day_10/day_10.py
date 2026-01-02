bc4ac734ebd98b2a7d3b600d2684887fcff4be02
Belgua Whale <belguawhale@gmail.com>
2026-01-02 14:59:48 -0500

    day 10 part 2 scaffolding

--- 2025/day_10/day_10.py
+++ 2025/day_10/day_10.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple, Set, Dict
+import numpy as np
 from collections import deque
 import math
 import re
@@ -58,7 +59,6 @@ def part_1(inp: InputType, debug=False):
     # idea: machine state is like a bitstring with 0 being off and 1 being on.
     # each button does an xor with machine state, where 1 toggles a light at index i and 0 leaves it
     # use iterated bfs to determine the shortest path from the 0 state to each machine state, until we find the target state
-    # i'm guessing part 2 will add weights to the graph edges, bfs assumes they're equal to 1.
     total = 0
     for machine in inp:
         total += process_machine(machine, debug)
@@ -92,27 +92,71 @@ def test(cases, fn):
 
 test(test_cases_part_1, part_1)
 
-exit()
-
 print("===== Part 2 =====")
 
-parse_file2 = parse_file
+# num_lights, buttons, joltage_reqs
+Machine2 = Tuple[int, List[np.typing.NDArray[np.int16]], np.typing.NDArray[np.int16]]
+InputType2 = List[Machine2]
 
 
-def part_2(inp: InputType, debug=False):
+def parse_file2(file: str) -> InputType2:
+    machines = []
+    with open(file, "r") as f:
+        for line in f:
+            state, *buttons, joltage = line.strip().split(" ")
+            target_state_str = state.strip("[]").replace(".", "0").replace("#", "1")
+            num_lights = len(target_state_str)
+            button_vals = []
+            for button in buttons:
+                vals = np.zeros(num_lights, dtype=np.int16)
+                for light in button.strip("()").split(","):
+                    vals[int(light)] = 1
+                button_vals.append(vals)
+            joltage_reqs = np.array(
+                [int(j) for j in joltage.strip("{}").split(",")], dtype=np.int16
+            )
+            machines.append(
+                (num_lights, int(target_state_str, 2), button_vals, joltage_reqs)
+            )
+    return machines
+
+
+def process_machine2(machine: Machine2, debug=False) -> int:
+    num_lights, buttons, joltage_reqs = machine
+
+    # Each button is like a vector in a spanning set for num_lights-D space.
+    # A solution is a linear combination of buttons that equal the joltage req
+    # with the constraint that
+    # 1. each coefficient is non-negative, and
+    # 2. we optimize for the smallest sum of coefficients
+
+    # max num_lights is about 10, max joltage reqs is about 300.
+    return 0
+
+
+def part_2(inp: InputType2, debug=False):
     if debug:
         pdb.set_trace()
+    total = 0
+    for machine in inp:
+        total += process_machine2(machine, debug)
+    return total
 
 
 example = parse_file2("input_example.txt")
 print(part_2(example))
 
-inp = parse_file2("input.txt")
-print(part_2(inp))
+# inp = parse_file2("input.txt")
+# print(part_2(inp))
 
 
 test_cases_part_2 = [
-    [parse_file2("input_example.txt"), 24],
+    [[[1, [np.array([1])], np.array([2])]], 2],
+    [[[2, [np.array([1, 0]), np.array([0, 1])], np.array([1, 1])]], 2],
+    # [[example[0]], 10],
+    # [[example[1]], 12],
+    # [[example[2]], 11],
+    # [parse_file2("input_example.txt"), 33],
 ]
 
 
